d0a50966bc8dfc186467494612e441d48b1db100
Spence Guo <37296692+SpenceGuo@users.noreply.github.com>
2019-12-06 15:13:54 +0800

    Update database.php

--- database.php
+++ database.php
@@ -4,7 +4,48 @@ class dbconfig
 	public static $host = '202.120.36.29';
 	public static $port = '23306';
 	public static $username = 'qi';
-	public static $password = 'password'
+	public static $password = 'password';
+	public static $dbname = 'SH_Stock_Exchange';
+	
+	public function hostValue()
+	{
+		return self::$host;
+	}
+	
+	public function portValue()
+	{
+		return self::$port;
+	}
+	
+	public function usernameValue()
+	{
+		return self::$username;
+	}
+	
+	public function passwordValue()
+	{
+		return self::$password;
+	}
+	
+	public function dbnameValue()
+	{
+		return self::$dbname;
+	}
 }
+
+// database connection test......
+$db = new dbconfig();
+$host = $db->hostValue().':'.$db->portValue();
+$conn = new mysqli($host,$db->usernameValue(),$db->passwordValue());
+
+if ($conn->connect_error) 
+{
+    die("连接失败: " . $conn->connect_error);
+}
+else{
+	echo "连接成功<br />";
+}
+
+print $db->hostValue();
 
 ?>
