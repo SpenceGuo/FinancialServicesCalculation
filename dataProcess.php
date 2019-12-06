288bceb284f396f268bbfe7c5490646767b7fe2f
Spence Guo <37296692+SpenceGuo@users.noreply.github.com>
2019-12-06 22:28:39 +0800

    update

--- dataProcess.php
+++ dataProcess.php
@@ -14,25 +14,25 @@ else{
 	echo "连接成功<br />";
 }
 
-$sql = 'SELECT securityid,datetime FROM his_cff WHERE securityid=\'IC0001\' AND datetime>20190918 ORDER BY datetime DESC';
-print($sql);
-
-$result = mysql_query($sql);
-
-if (mysql_num_rows($result) > 0) {
-    // 输出数据
-    $arr = mysql_fetch_array($result);
-	print_r($arr);
+//$sql = 'SELECT securityid,datetime FROM his_cff WHERE securityid=\'IC0001\' AND datetime>20190918 ORDER BY datetime DESC';
+//print($sql);
+//
+//$result = mysql_query($sql);
+//
+//if (mysql_num_rows($result) > 0) {
+//  // 输出数据
+//  $arr = mysql_fetch_array($result);
+//	print_r($arr);
 //	$arr_securityid = $arr["securityid"];
 //	$arr_datetime = $arr["datetime"];
 	
 //  while($row = $result->fetch_assoc()) {
 //      echo "securityid: " . $row["securityid"]. " - datetime: " . $row["datetime"]."<br>";
 //  }
-} else {
-    echo "0 结果";
-}
-$conn->close();
+//} else {
+//  echo "0 结果";
+//}
+//$conn->close();
 
 
 echo "<br />";
