66b417f7ac27ed5c06b205fb863e34bb57652be5
Spence Guo <37296692+SpenceGuo@users.noreply.github.com>
2019-12-11 17:02:45 +0800

    up

--- dataProcess.php
+++ dataProcess.php
@@ -1,50 +1,43 @@
 <?php
 // database connection ...
-require('database.php');
-$db = new dbconfig();
-//$host = $db->hostValue().':'.$db->portValue();
-$host = $db->hostValue();
-$conn = new mysqli($host,$db->usernameValue(),$db->passwordValue());
+//require('database.php');
+//$db = new dbconfig();
+//
+//$host = $db->hostValue();
+//$username = $db->usernameValue();
+//$password = $db->passwordValue();
+//$dbname = $db->dbnameValue();
 
-if ($conn->connect_error) 
-{
+$conn = new mysqli("10.10.11.2","qi","password","SH_Stock_Exchange");
+
+// Check connection
+if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
+} 
+   
+$sql = "SELECT securityid,datetime FROM his_cff WHERE securityid=\'IC0001\' AND datetime>20190918 ORDER BY datetime DESC";
+$result = $conn->query($sql);
+   
+if ($result->num_rows > 0) {
+    // 输出数据
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["securityid"]. " - Name: " . $row["datetime"]. "<br>";
+    }
+} else {
+    echo "0 结果";
 }
-else{
-	echo "连接成功<br />";
-}
+$conn->close();
 
-//$sql = 'SELECT securityid,datetime FROM his_cff WHERE securityid=\'IC0001\' AND datetime>20190918 ORDER BY datetime DESC';
-//print($sql);
+//echo "<br />";
+
+//$stockData = array(
+//array(1,2,3),
+//array(4,5,6),
+//array(7,8,9)
+//);
+//$arr = json_encode($stockData);
 //
-//$result = mysql_query($sql);
-//
-//if (mysql_num_rows($result) > 0) {
-//  // 输出数据
-//  $arr = mysql_fetch_array($result);
-//	print_r($arr);
-//	$arr_securityid = $arr["securityid"];
-//	$arr_datetime = $arr["datetime"];
-	
-//  while($row = $result->fetch_assoc()) {
-//      echo "securityid: " . $row["securityid"]. " - datetime: " . $row["datetime"]."<br>";
-//  }
-//} else {
-//  echo "0 结果";
-//}
-//$conn->close();
-
-
-echo "<br />";
-
-$stockData = array(
-array(1,2,3),
-array(4,5,6),
-array(7,8,9)
-);
-$arr = json_encode($stockData);
-
-print_r($arr);
+//print_r($arr);
 
 
 ?>
--- new_file.php
+++ new_file.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "10.10.11.2";
-$username = "qi";
-$password = "password";
-$dbname = "SH_Stock_Exchange";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "financial";
+//$dbname = "SH_Stock_Exchange";
  //echo "securityid: " . $row["securityid"]. " - datetime: " . $row["datetime"]. "<br>";
- 
  // 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -12,13 +12,14 @@ if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 } 
  
-$sql = "SELECT securityid,datetime FROM his_cff WHERE securityid=\'IC0001\' AND datetime>20190918 ORDER BY datetime DESC";
+//$sql = "SELECT securityid,datetime FROM his_cff WHERE securityid=\'IC0001\' AND datetime>20190918 ORDER BY datetime DESC";
+$sql = "SELECT * FROM data";
 $result = $conn->query($sql);
  
 if ($result->num_rows > 0) {
     // 输出数据
     while($row = $result->fetch_assoc()) {
-        echo "securityid: " . $row["securityid"]. " - datetime: " . $row["datetime"]. "<br>";
+        echo "securityid: " . $row["datetime"]. " - datetime: " . $row["value"]. "<br>";
     }
 } else {
     echo "0 结果";
